ddfb795a8db708a0dfe26184232694ec2ee53047
Ajax12345 <jpetullo14@gmail.com>
2023-09-09 14:49:23 -0400

    updates

--- actor_genotype.py
+++ actor_genotype.py
@@ -1,37 +1,39 @@
 import random, typing, json
+import collections
 
 class node:
-    class _val:
-        def __init__(self, value) -> None:
-            self.value = value
-
-        def __repr__(self) -> str:
-            return f'Value({self.value})'
-        
     class Input:
-        def __init__(self, _type:typing.Type, name:typing.Union[str, int] = None) -> None:
+        def __init__(self, _type:typing.Type, name:typing.Union[str, int], value:typing.Any = None) -> None:
             self._type = _type
             self.name = name
+            self.value = None if value is None else self(value)
 
-        def __call__(self, val:typing.Any) -> '_val':
+        def __call__(self, val:typing.Any) -> typing.Any:
             if not isinstance(val, self._type):
                 raise TypeError
             
-            return node._val(val)
+            return val
+
+        def set_value(self, value) -> None:
+            self.value = self(value)
 
         def __repr__(self) -> str:
             return f'node.{self.__class__.__name__}({self._type.__name__}, {self.name})'
 
     class Constant:
-        def __init__(self, _type:typing.Type, name:typing.Union[str, int] = None) -> None:
+        def __init__(self, _type:typing.Type, name:typing.Union[str, int], value:typing.Any = None) -> None:
             self._type = _type
             self.name = name
+            self.value = None if value is None else self(value)
 
-        def __call__(self, val:typing.Any) -> '_val':
+        def set_value(self, value) -> None:
+            self.value = self(value)
+
+        def __call__(self, val:typing.Any) -> typing.Any:
             if not isinstance(val, self._type):
                 raise TypeError
             
-            return node._val(val)
+            return val
 
         def __repr__(self) -> str:
             return f'node.{self.__class__.__name__}({self._type.__name__}, {self.name})'
@@ -41,12 +43,17 @@ class node:
             self._type = _type
             self.name = name
             self.input = _input
+            self.value = None
+            self.parent_path = None
 
-        def __call__(self, val:typing.Any) -> '_val':
+        def set_value(self, value) -> None:
+            self.value = self(value)
+
+        def __call__(self, val:typing.Any) -> typing.Any:
             if not isinstance(val, self._type):
                 raise TypeError
             
-            return node._val(val)
+            return val
 
         def __repr__(self) -> str:
             return f'node.{self.__class__.__name__}({self._type.__name__}, {self.name})'
@@ -56,14 +63,77 @@ class node:
             def __init__(self, name:typing.Union[str, int], inputs:typing.List = []) -> None:
                 self.name = name
                 self.inputs = inputs
+                self.parents = []
+
+            def __call__(self, *inputs) -> typing.Any:
+                assert len(inputs) == len(self.inputs)
+                return not all(inputs)
         
             def __repr__(self) -> str:
-                return f'node.operator.{self.__class__.__name__}({self.name}, inputs={self.inputs})'
+                return f'node.operator.{self.__class__.__name__}({self.name}, inputs={self.inputs}, parents={self.parents})'
 
 class Genotype:
     def __init__(self, **kwargs:dict) -> None:
         self.kwargs = kwargs
-    
+        self.gate_bindings = {j.name:j for j in kwargs['gates']}
+        self.value_bindings = None    
+
+    def __enter__(self) -> 'Genotype':
+        return
+
+    def __exit__(self, *_) -> None:
+        self.value_bindings = None
+        for i in self.gate_bindings.values():
+            i.parents = []
+
+        for i in self.kwargs['inputs']:
+            i.value = None
+
+
+    def __call__(self, *traits) -> typing.Any:
+        assert len(traits) == len(self.kwargs['inputs'])
+
+        for val, inp in zip(traits, self.kwargs['inputs']):
+            inp.set_value(val)
+
+        self.traverse()
+        return [self.value_bindings[i.input] for i in self.kwargs['outputs']]
+
+    @property
+    def complexity(self) -> int:
+        if self.value_bindings is None:
+            self.traverse()
+        
+        parent_gates = set()
+        for i in self.kwargs['outputs']:
+            if i.input in self.gate_bindings:
+                for j in self.gate_bindings[i.input].parents:
+                    parent_gates.add(j)
+
+        print('parent gates', parent_gates)
+
+        return len(parent_gates)
+
+
+    def traverse(self) -> None:
+        values = {**{i.name:i.value for i in self.kwargs['inputs']}, 
+                **{i.name:i.value for i in self.kwargs['constants']}}
+        
+        print(values)
+        seen = []
+        while (queue:=[gate for gate in self.kwargs['gates'] if all(i in values for i in gate.inputs) and gate.name not in seen]):
+            for gate in queue:
+                seen.append(gate.name)
+                values[gate.name] = gate(*[values[i] for i in gate.inputs])
+                for i in gate.inputs:
+                    if i in self.gate_bindings:
+                        gate.parents = {*gate.parents, *self.gate_bindings[i].parents, i}
+                    else:
+                        gate.parents = {*gate.parents, i}
+            
+        self.value_bindings = values
+        print(self.value_bindings)
+        
     def __repr__(self) -> str:
         return json.dumps({a:[*map(repr, b)] for a, b in self.kwargs.items()}, indent=4)
 
@@ -76,8 +146,8 @@ if __name__ == '__main__':
             node.Input(int, 2)
         ],
         constants = [
-            node.Constant(int, 3),
-            node.Constant(int, 4)
+            node.Constant(int, 3, value = 0),
+            node.Constant(int, 4, value = 1)
         ],
         gates = [
             node.operator.NAND(5, inputs = [0, 1]),
@@ -89,5 +159,8 @@ if __name__ == '__main__':
         ],
         outputs = [node.Output(int, 11, 10)]
     )
-            
-    print(g)
+
+    with g:     
+        print(g(0, 0, 1))
+        print('complexity', g.complexity)
+
