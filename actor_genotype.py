50405fb0501106f94fea7cb958ec781866986fae
Ajax12345 <jpetullo14@gmail.com>
2023-09-09 13:22:42 -0400

    basic wrappers for genotype added

--- actor_genotype.py
+++ actor_genotype.py
@@ -1,4 +1,4 @@
-import random, typing
+import random, typing, json
 
 class node:
     class _val:
@@ -37,9 +37,10 @@ class node:
             return f'node.{self.__class__.__name__}({self._type.__name__}, {self.name})'
 
     class Output:
-        def __init__(self, _type:typing.Type, name:typing.Union[str, int] = None) -> None:
+        def __init__(self, _type:typing.Type, name:typing.Union[int, float], _input:int) -> None:
             self._type = _type
             self.name = name
+            self.input = _input
 
         def __call__(self, val:typing.Any) -> '_val':
             if not isinstance(val, self._type):
@@ -52,21 +53,41 @@ class node:
 
     class operator:
         class NAND:
-            def __init__(self, name:typing.Union[str, int] = None, input_num:int, inputs:typing.List) -> None:
+            def __init__(self, name:typing.Union[str, int], inputs:typing.List = []) -> None:
                 self.name = name
-                self.input_num = input_num
                 self.inputs = inputs
         
             def __repr__(self) -> str:
-                return f'node.operator.{self.__class__.__name__}({self.name})'
+                return f'node.operator.{self.__class__.__name__}({self.name}, inputs={self.inputs})'
 
 class Genotype:
     def __init__(self, **kwargs:dict) -> None:
-        pass
+        self.kwargs = kwargs
+    
+    def __repr__(self) -> str:
+        return json.dumps({a:[*map(repr, b)] for a, b in self.kwargs.items()}, indent=4)
 
 
 if __name__ == '__main__':
     g = Genotype(
-
+        inputs = [
+            node.Input(int, 0),
+            node.Input(int, 1),
+            node.Input(int, 2)
+        ],
+        constants = [
+            node.Constant(int, 3),
+            node.Constant(int, 4)
+        ],
+        gates = [
+            node.operator.NAND(5, inputs = [0, 1]),
+            node.operator.NAND(6, inputs = [2, 3]),
+            node.operator.NAND(7, inputs = [5, 1]),
+            node.operator.NAND(8, inputs = [2, 4]),
+            node.operator.NAND(9, inputs = [7, 7]),
+            node.operator.NAND(10, inputs = [7, 8])
+        ],
+        outputs = [node.Output(int, 11, 10)]
     )
             
+    print(g)
