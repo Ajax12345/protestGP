1f27d8f8b2716a06fa77bde45c7a7f73bae2c222
Ajax12345 <jpetullo14@gmail.com>
2023-09-25 19:42:09 -0400

    updates

--- boolean_solver.py
+++ boolean_solver.py
@@ -1,126 +1,144 @@
-import collections, typing
+import collections, typing, copy
 
 class operators:
+    OPS = [
+        'AND', 'OR', 'NAND', 'NOR'
+    ]
     class AND:
-        def __init__(self, a, b) -> None:
-            self.a, self.b = a, b
+        def __init__(self, *args) -> None:
+            self.container = [*args]
+
+        def AND(self, _m) -> typing.Any:
+            if _m.__class__.__name__ not in operators.OPS:
+                return operators.AND(*self.container, _m)
+
+            if isinstance(_m, self.__class__):
+                return operators.AND(*self.container, *_m.container)
+
+            return _m.AND(self)
+
+        def OR(self, _m) -> typing.Any:
+            return operators.OR(self, _m)
 
         def __repr__(self) -> str:
-            return f'({self.a} * {self.b})'
-
+            return '('+' * '.join(map(str, self.container))+')'
+        
         def __str__(self) -> str:
             return repr(self)
-
-        def __getattr__(self, _f_name) -> typing.Callable:
-            def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
-            
-            return wrapper
 
     class OR:
-        def __init__(self, a, b) -> None:
-            self.a, self.b = a, b
+        def __init__(self, *args) -> None:
+            self.container = [*args]
 
-        def __repr__(self) -> str:
-            return f'({self.a} + {self.b})'
-
-        def __str__(self) -> str:
-            return repr(self)
-
-        def __getattr__(self, _f_name) -> typing.Callable:
-            def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
+        def AND(self, _m) -> typing.Any:
+            result = []
+            for i in self.container:
+                if isinstance(t:=i.AND(_m), self.__class__):
+                    result.extend(t.container)
+                else:
+                    result.append(t)
             
-            return wrapper
-
-    class NAND:
-        def __init__(self, a, b) -> None:
-            self.a, self.b = a, b
-
-        def __repr__(self) -> str:
-            return f'[({self.a}) * ({self.b})]'
-
-        def __str__(self) -> str:
-            return repr(self)
-
-        def __getattr__(self, _f_name) -> typing.Callable:
-            def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
-            
-            return wrapper
-
-    class NOR:
-        def __init__(self, a, b) -> None:
-            self.a, self.b = a, b
-
-        def __repr__(self) -> str:
-            return f'[({self.a}) + ({self.b})]'
-
-        def __str__(self) -> str:
-            return repr(self)
+            return operators.OR(*result)
         
-        def __getattr__(self, _f_name) -> typing.Callable:
-            def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
-            
-            return wrapper
+        def OR(self, _m) -> typing.Any:
+            return operators.OR(*self.container, _m)
 
+        def __repr__(self) -> str:
+            return '('+' + '.join(map(str, self.container))+')'
+        
+        def __str__(self) -> str:
+            return repr(self)
+
+class entities:
     class One:
         def __repr__(self) -> str:
-            return '1'
+            return '<1>'
 
         def __getattr__(self, _f_name) -> typing.Callable:
             def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
+                return getattr(entities, _f_name)(self, _m)
             
             return wrapper
+
+        def AND(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.AND(self, _m)
+
+            return _m.AND(self)
+
+        def OR(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.OR(self, _m)
+
+            return _m.OR(self)
 
         def __str__(self) -> str:
             return repr(self)
 
-        def not_(self) -> 'Zero':
-            return operators.Zero()
+        def NOT(self) -> 'Zero':
+            return entities.Zero()
 
     class Zero:
         def __repr__(self) -> str:
-            return '0'
+            return '<0>'
 
         def __getattr__(self, _f_name) -> typing.Callable:
             def wrapper(_m:'M') -> 'operator':
-                return getattr(operators, _f_name)(self, _m)
+                return getattr(entities, _f_name)(self, _m)
             
             return wrapper
+
+        def AND(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.AND(self, _m)
+
+            return _m.AND(self)
+
+        def OR(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.OR(self, _m)
+
+            return _m.OR(self)
 
         def __str__(self) -> str:
             return repr(self)
 
-        def not_(self) -> 'One':
-            return operators.One()
+        def NOT(self) -> 'One':
+            return entities.One()
 
-class M:
-    def __init__(self, _id:typing.Any) -> None:
-        self._id = _id
-        self._not = False
+    class M:
+        def __init__(self, _id:typing.Any) -> None:
+            self._id = _id
+            self._not = False
 
-    def not_(self) -> 'M':
-        self._not = not self._not
-        return self
+        def AND(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.AND(self, _m)
 
-    def __str__(self) -> str:
-        return repr(self)
+            return _m.AND(self)
 
-    def __repr__(self) -> str:
-        return ['', 'not '][self._not] + str(self._id)
+        def OR(self, _m) -> typing.Any:
+            if isinstance(_m, self.__class__):
+                return operators.OR(self, _m)
 
-    def __getattr__(self, _f_name) -> typing.Callable:
-        def wrapper(_m:'M') -> 'operator':
-            return getattr(operators, _f_name)(self._id, _m)
-        
-        return wrapper
+            return _m.OR(self)
+
+        def NOT(self) -> 'M':
+            self._not = not self._not
+            return self
+
+        def __str__(self) -> str:
+            return repr(self)
+
+        def __repr__(self) -> str:
+            return ['', 'not '][self._not] + str(self._id)
 
 if __name__ == '__main__':
-    print(M(1).AND(M(2)).OR(M(3)).NOR(M(4)))
-    
+    M = entities.M
+    a = M(1).OR(M(2)).OR(M(3)).OR(M(4))
+    b = M(5).OR(M(6)).OR(M(7)).OR(M(8))
+    print(a, b)
+    print(a.AND(b))    
 
     
     
