488b3c55798491b2af5093cb22f2e78b248f4676
Ajax12345 <jpetullo14@gmail.com>
2023-09-25 20:49:42 -0400

    updates

--- boolean_solver.py
+++ boolean_solver.py
@@ -87,6 +87,11 @@ class operators:
             return repr(self)
 
 class entities:
+    ENTITIES = [
+        'M',
+        'One',
+        'Zero'
+    ]
     class One:
         def __repr__(self) -> str:
             return '1'
@@ -98,27 +103,27 @@ class entities:
             return wrapper
 
         def AND(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.AND(self, _m)
 
             return _m.AND(self)
 
         def NAND(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.OR(self.NOT(), _m.NOT())
             
             a = _m.NOT()
             return a.OR(_m.NOT())
 
         def NOR(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.AND(self.NOT(), _m.NOT())
 
             a = _m.NOT()
             return a.AND(_m.NOT())
 
         def OR(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.OR(self, _m)
 
             return _m.OR(self)
@@ -140,13 +145,27 @@ class entities:
             return wrapper
 
         def AND(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.AND(self, _m)
 
             return _m.AND(self)
 
+        def NAND(self, _m) -> typing.Any:
+            if _m.__class__.__name__ in entities.ENTITIES:
+                return operators.OR(self.NOT(), _m.NOT())
+            
+            a = _m.NOT()
+            return a.OR(_m.NOT())
+
+        def NOR(self, _m) -> typing.Any:
+            if _m.__class__.__name__ in entities.ENTITIES:
+                return operators.AND(self.NOT(), _m.NOT())
+
+            a = _m.NOT()
+            return a.AND(_m.NOT())
+
         def OR(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.OR(self, _m)
 
             return _m.OR(self)
@@ -163,19 +182,25 @@ class entities:
             self._not = _not
 
         def AND(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.AND(self, _m)
 
             return _m.AND(self)
 
         def NAND(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.OR(self.NOT(), _m.NOT())
             
             return _m.NAND(self)
 
+        def NOR(self, _m) -> typing.Any:
+            if _m.__class__.__name__ in entities.ENTITIES:
+                return operators.AND(self.NOT(), _m.NOT())
+            
+            return _m.NOR(self)
+
         def OR(self, _m) -> typing.Any:
-            if isinstance(_m, self.__class__):
+            if _m.__class__.__name__ in entities.ENTITIES:
                 return operators.OR(self, _m)
 
             return _m.OR(self)
@@ -191,12 +216,23 @@ class entities:
 
 if __name__ == '__main__':
     M = entities.M
-    
+    One = entities.One
+    Zero = entities.Zero
+    '''
+    print(One().AND(Zero()))
+    '''
+    '''
     b = M(5).AND(M(6)).AND(M(7)).OR(M(4).AND(M(9)))
     c = M(10).AND(M(11))
     print(b, ', ', c)
     print(b.NOT(), ', ', c.NOT())
     print(b.NOR(c))
+    '''
+    a = M(1).OR(M(2)).OR(M(3))
+    b = M(4).OR(M(5)).OR(M(6))
+    print(a, b, a.NOR(b))
+
+
     
     
     
